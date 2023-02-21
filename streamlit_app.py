d9bef117e93642a2cabaf6e2d4b6e23174cb7faa
PpathakGitHub36 <126014550+PpathakGitHub36@users.noreply.github.com>
2023-02-21 13:50:14 -0600

    Update streamlit_app.py
    added emojis and build your own bf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale , Spinach and Rocket Smoothie')
-streamlit.text('HARD-Boiled Free Range Egg')
+streamlit.header('🥙Breakfast Favourites')
+streamlit.text('🧈Omega 3 and Blueberry Oatmeal')
+streamlit.text('🍤Kale , Spinach and Rocket Smoothie')
+streamlit.text('🥚HARD-Boiled Free Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
